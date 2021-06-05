b0c83d351ac4832eda529808eb3019f41cd5e0bb
CommudeVN Nam <nguyenhoainam.090801@gmail.com>
2021-06-05 09:01:12 +0700

    new update

---
+++ app/src/main/java/com/example/ditimtrieuphu/entity/Question.java
@@ -0,0 +1,12 @@
+package com.example.ditimtrieuphu.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class Question {
+
+}
---
+++ app/src/main/java/com/example/ditimtrieuphu/view/adapter/QuestionAdapter.java
@@ -0,0 +1,4 @@
+package com.example.ditimtrieuphu.view.adapter;
+
+public class QuestionAdapter {
+}
