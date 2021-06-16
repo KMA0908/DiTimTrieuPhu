54b2a8e930a679a81f383947b515588e566c265c
rolandonogoldgoal <68616009+rolandonogoldgoal@users.noreply.github.com>
2021-06-16 16:47:48 +0700

    upadate query

--- app/src/main/java/com/example/ditimtrieuphu/viewmodel/QuestionViewModel.java
+++ app/src/main/java/com/example/ditimtrieuphu/viewmodel/QuestionViewModel.java
@@ -1,23 +1,6 @@
 package com.example.ditimtrieuphu.viewmodel;
 
-import android.content.Context;
-
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.ditimtrieuphu.entity.Question;
-import com.example.ditimtrieuphu.roomdatabse.AppDatabase;
-import com.example.ditimtrieuphu.roomdatabse.Repository;
-
-import java.util.List;
-
 public class QuestionViewModel extends ViewModel {
-    public static List<Question> allQuestions;
-
-    public static Repository repository;
-
-    public QuestionViewModel(Context context) {
-         repository= new Repository(context);
-         allQuestions = repository.getAllQuestions();
-    }
 }
