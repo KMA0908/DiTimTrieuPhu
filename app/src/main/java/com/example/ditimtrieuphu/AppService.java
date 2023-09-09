b82235a135e8967688fd11506d317e8a90b632f6
nam09082001 <106221921+nam09082001@users.noreply.github.com>
2023-09-09 12:53:10 +0700

    fix crash multi thread

---
+++ app/src/main/java/com/example/ditimtrieuphu/AppService.java
@@ -0,0 +1,85 @@
+package com.example.ditimtrieuphu;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Minh: them service cho app
+ */
+public class AppService extends Service {
+    private IBinder mBinder;
+    private MediaPlayer mBackgroundMediaPlayer;
+    private boolean mBgMusicUserSetting = true; // on/off bg music theo user
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mBinder = new AppServiceBinder();
+        mBackgroundMediaPlayer = new MediaPlayer();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    /**
+     * Bat dau phat mot sound moi
+     * @param soundId: res cua sound
+     * @param looping: loop nhac hay khong
+     */
+    public void startBackgroundMusic(int soundId, boolean looping) {
+        setBackgroundMusic(soundId, looping);
+        startBackgroundMusic();
+    }
+
+    /**
+     * Bat dau phat nhac, su dung nhac da duoc set truoc do
+     */
+    public void startBackgroundMusic() {
+        Log.d("MinhNTn", "startBackgroundMusic: 1");
+        if (!mBackgroundMediaPlayer.isPlaying()) {
+            Log.d("MinhNTn", "startBackgroundMusic: 2");
+            mBackgroundMediaPlayer.setOnPreparedListener(mediaPlayer -> {
+                Log.d("MinhNTn", "startBackgroundMusic: ");
+                mediaPlayer.start();
+            });
+        }
+    }
+
+    public void setBgMusicUserSetting(boolean bgPlaying) {
+        this.mBgMusicUserSetting = bgPlaying;
+    }
+    public boolean isBgMusicUserSetting() {
+        return mBgMusicUserSetting;
+    }
+
+    public void pauseBackgroundMusic() {
+        mBackgroundMediaPlayer.pause();
+    }
+
+    public void stopBackgroundMusic() {
+        mBackgroundMediaPlayer.stop();
+    }
+
+    /**
+     * Set file mp3 cho nhac nen
+     */
+    private void setBackgroundMusic(int soundId, boolean looping) {
+        mBackgroundMediaPlayer = MediaPlayer.create(this, soundId);
+        mBackgroundMediaPlayer.setLooping(looping);
+    }
+
+    public class AppServiceBinder extends Binder {
+        public AppService getAppService() {
+            return AppService.this;
+        }
+    }
+}
